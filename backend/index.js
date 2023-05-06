7aaa53ed0b7f1416f67a683b9a4e9f159c12ed49
aum <techtv49@gmail.com>
2023-05-06 10:21:17 +0530

    commit

---
+++ backend/index.js
@@ -0,0 +1,41 @@
+import { Configuration, OpenAIApi } from "openai";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+const port = 1234;
+app.use(bodyParser.json());
+app.use(cors());
+
+const configuration = new Configuration({
+  organization: "Personal",
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+app.post("/", async (request, response) => {
+  const { chats } = request.body;
+
+  const result = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are a EbereGPT. You can help with graphic design tasks",
+      },
+      ...chats,
+    ],
+  });
+
+  response.json({
+    output: result.data.choices[0].message,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
+// app.get("/", (request, response) => {
+//   response.send("i am working!");
+// });
